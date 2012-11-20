c14a764f9117227baa00a4ec333ae4d98dda52c9
Robert McGibbon <rmcgibbo@gmail.com>
2012-11-19 18:16:31 -0800

    Initial Commit

---
+++ objc-zmq-frame/objc_zmq_frame.h
@@ -0,0 +1,13 @@
+//
+//  objc_zmq_frame.h
+//  objc-zmq-frame
+//
+//  Created by Robert McGibbon on 11/19/12.
+//  Copyright (c) 2012 Stanford University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface objc_zmq_frame : NSObject
+
+@end
---
+++ objc-zmq-frame/objc_zmq_frame.m
@@ -0,0 +1,13 @@
+//
+//  objc_zmq_frame.m
+//  objc-zmq-frame
+//
+//  Created by Robert McGibbon on 11/19/12.
+//  Copyright (c) 2012 Stanford University. All rights reserved.
+//
+
+#import "objc_zmq_frame.h"
+
+@implementation objc_zmq_frame
+
+@end
